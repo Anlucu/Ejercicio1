8ddfa4968f2a313475bac46e24b4c5cd65d87609
Anlucu <124319781+Anlucu@users.noreply.github.com>
2023-03-04 12:18:40 -0300

    Create archivo.py

---
+++ archivo.py
@@ -0,0 +1,20 @@
+import pickle
+
+
+class vehiculo():
+
+    def __init__(self):
+        self.auto = "Camaro"
+        self.ruedas = 4
+        self.puertas = 4
+
+
+auto = vehiculo()
+
+f = open("clase", "wb")
+pickle.dump(auto, f)
+f.close()
+del(f)
+
+f = open("clase", "rb")
+lista = pickle.load(f)
